a227c011592d0ad9f5afab06235e2e40f6121756
Serega8 <kulchytski88@yandex.by>
2014-01-22 19:52:04 +0300

    index.php add helpers

--- application/views/client/index.php
+++ application/views/client/index.php
@@ -6,8 +6,8 @@
         <img class="s-images" src="<?= $slide['img_url']; ?>" alt="<?= $slide['img_alt']; ?>" title="<?= $slide['img_title']; ?>"/>
 			</div>	
          <? endforeach; ?>
-			<a class="carousel-control left" href="#mycarousel" data-slide="prev"> &lsaquo; </a>
-			<a class="carousel-control right" href="#mycarousel" data-slide="next"> &rsaquo; </a>
+                        <?=HTML::anchor('#mycarousel', '&lsaquo;', array('class' => 'carousel-control left', 'data-slide' => 'prev'));?>
+                        <?=HTML::anchor('#mycarousel', '&rsaquo;', array('class' => 'carousel-control right', 'data-slide' => 'next'));?>
 			<ol class="carousel-indicators">
 				<li class="active" data-target="#mycarousel" data-slide-to="0"> </li>
 				<li data-target="#mycarousel" data-slide-to="1"></></li>
@@ -24,35 +24,41 @@
 					<h2>картриджей</h2>
 					<hr>
 					<p>dyhdhyhrdyh sdfjhn dsth dfhrfthrftgh rfth dsrthyug swrt e yhj tdyhtg rttg tg detg eatgetg edgtvget5 </p>
-					<a href="#"><img src="content/images/1c.jpg"></a>
+					<!--<a href="#"><img src="content/images/1c.jpg"></a>-->
+                                        <?=HTML::anchor('#', '<img src="content/images/1c.jpg">');?>
 				</li>
 				<li id="blue">
 					<h1>Монтаж</h1>
 					<h2>локальных сетей</h2>
 					<hr>
 					<p>dyhrdyh sdfjhn dsth dfhrfthrftgh rfth dsrthyug swrt e yhj tdyhtg rttg tg detg eatgetg edgtvget5</p>
-					<a href="#"><img src="content/images/2c.jpg"></a>
+					<!--<a href="#"><img src="content/images/2c.jpg"></a>-->
+                                        <?=HTML::anchor('#', '<img src="content/images/2c.jpg">');?>
 				</li>
 				<li id="pink">
 					<h1>Серверное</h1>
 					<h2>оборудование</h2>
 					<hr>
 					<p> edrgyhjdyhrdyh sdfjhn dsth dfhrfthrftgh rfth dsrthyug swrt e yhj tdyhtg rttg tg detg eatgetg edgtvget5u</p>
-					<a href="#"><img src="content/images/3c.jpg"></a>
+					<!--<a href="#"><img src="content/images/3c.jpg"></a>-->
+                                        <?=HTML::anchor('#', '<img src="content/images/3c.jpg">');?>
+                                        
 				</li>
 				<li id="yellow">
 					<h1>Восстановление</h1>
 					<h2>жестких дисков</h2>
 					<hr>
 					<p> srtyrdyhrdyh sdfjhn dsth dfhrfthrftgh rfth dsrthyug swrt e yhj tdyhtg rttg tg detg eatgetg edgtvget5ty</p>
-					<a href="#"><img src="content/images/4c.jpg"></a>
+					<!--<a href="#"><img src="content/images/4c.jpg"></a>-->
+                                        <?=HTML::anchor('#', '<img src="content/images/4c.jpg">');?>
 				</li>
 				<li id="green">
 					<h1>Каталог</h1>
 					<h2>товаров</h2>
 					<hr>
 					<p>cfgdyhrdyh sdfjhn dsth dfhrfthrftgh rfth dsrthyug swrt e yhj tdyhtg rttg tg detg eatgetg edgtvget5yujh </p>
-					<a href="#"><img src="content/images/5c.jpg"></a>
+					<!--<a href="#"><img src="content/images/5c.jpg"></a>-->
+                                        <?=HTML::anchor('#', '<img src="content/images/5c.jpg">');?>
 				</li>
 			</ul>
 		</section>
@@ -80,7 +86,7 @@
 				<div class="otzyvy" >
 					<h3>Отзывы </h3> 
 					<p> Отличная компания, рабо уже третий год lorem ipsumОтличная компания, работаем вместе уже третий год lorem ipsumОтличная компания, работаем вместе уже третий год lorem ipsumОтличная компания, работаем вместе уже третий год lorem ipsumlorem ipsumОтличная компания, работаем вместе уже третий год lorem ipsum</p>
-					<p class="dalee1"><a href="#"> Читать далее </a><p>		
+					<p class="dalee1"><?=HTML::anchor('#', 'Читать далее');?><p>		
 				</div> 	
                             <!--ОТЗЫВЫ КОНЕЦ-->
 			</section>
@@ -90,31 +96,33 @@
 				<h3 id="news">Новости </h3>
 				<ul> 
 					<li><div class="pic"> 
-						<img src="content/images/1v.jpg" width="110px" height="80px"/> </div>
+                                                <?=HTML::image('content/images/1v.jpg', array('width' => '110px', 'height' => '80px'));?></div>
 						<div class="text">
 						<h3>01.09.2013</h3>
 						<p>  lorem ipsumlorem ipsumОтличная компания, работаем вместе уже третий год lorem ipsum</p>
 						</div>
 						<div class="clear"> </div>
-						<p class="dalee"> <a href="#"> Читать далее </a></p>
+						<p class="dalee"> 
+                                                     <?=HTML::anchor('#', 'Читать далее');?>
+                                                </p>
 					</li>
 					<li> <div class="pic"> 
-						<img src="content/images/2v.jpg" width="110px" height="80px"/> </div>
+						<?=HTML::image('content/images/2v.jpg', array('width' => '110px', 'height' => '80px'));?></div>
 						<div class="text">
 						<h3>01.09.2013</h3>
 						<p>  lorem ipsumlorem ipsumОтличная компания, работаем вместе уже третий год lorem ipsum</p>
 						</div>
 						<div class="clear"> </div>
-						<p class="dalee"><a href="#"> Читать далее </a></p>	
+						<p class="dalee"><?=HTML::anchor('#', 'Читать далее');?></p>	
 					</li>
 					<li> <div class="pic"> 
-						<img src="content/images/3v.jpg" width="110px" height="80px"/> </div>
+                                                <?=HTML::image('content/images/3v.jpg', array('width' => '110px', 'height' => '80px'));?></div>
 						<div class="text">
 						<h3>01.09.2013</h3>
 						<p>  lorem ipsumlorem ipsumОтличная компания, работаем вместе уже третий год lorem ipsum</p>
 						</div>
 						<div class="clear"> </div>
-						<p class="dalee"><a href="#"> Читать далее </a></p>	
+						<p class="dalee"><?=HTML::anchor('#', 'Читать далее');?></p>	
 					</li>
 				</ul>				
 			</aside>
@@ -126,15 +134,15 @@
 		<section class="smallslider">
 			<div id="myCarousel2" class="carousel slide">				
 				<div class="carousel-inner">
-					<div class="active item"><img src="content/images/panasonic.jpg"></div>
-					<div class="item"><img src="content/images/canon.jpg"></div>
-					<div class="item"><img src="content/images/lexmark.jpg"></div>
-					<div class="item"><img src="content/images/kuocera.jpg"></div>
-					<div class="item"><img src="content/images/xerox.jpg"></div>
-					<div class="item"><img src="content/images/hp.jpg"></div>
+					<div class="active item"><?=HTML::image('content/images/panasonic.jpg');?></div>
+					<div class="item"><?=HTML::image('content/images/canon.jpg');?></div>
+					<div class="item"><?=HTML::image('content/images/lexmark.jpg');?></div>
+					<div class="item"><?=HTML::image('content/images/kuocera.jpg');?></div>
+					<div class="item"><?=HTML::image('content/images/xerox.jpg');?></div>
+					<div class="item"><?=HTML::image('content/images/hp.jpg');?></div>
 				</div>				
-				<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-				<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                                <?=HTML::anchor('#myCarousel', '&lsaquo;', array('class' => 'carousel-control left', 'data-slide' => 'prev'));?>
+                                <?=HTML::anchor('#myCarousel', '&rsaquo;', array('class' => 'carousel-control right', 'data-slide' => 'next'));?>
 			</div>
 		</section>
              <!--НИЖНИЙ СЛАЙДЕР КОНЕЦ-->
