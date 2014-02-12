77b7c0ed05cde73ebbd8eb6242def920dd845714
serega8 <kulchytski88@yandex.by>
2014-02-12 19:51:05 +0300

    categ

--- application/logs/2014/02/12.php
+++ application/logs/2014/02/12.php
@@ -174,3 +174,19 @@
 #12 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
 #13 /home/intellectit/Документы/проекты/smartcopy.by/index.php(118): Kohana_Request->execute()
 #14 {main} in /home/intellectit/Документы/проекты/smartcopy.by/application/views/client/uslugi.php:27
+2014-02-12 19:31:58 --- EMERGENCY: ErrorException [ 8 ]: Undefined variable: subcategories ~ APPPATH/views/client/categories.php [ 2 ] in /home/intellectit/Документы/проекты/smartcopy.by/application/views/client/categories.php:2
+2014-02-12 19:31:58 --- DEBUG: #0 /home/intellectit/Документы/проекты/smartcopy.by/application/views/client/categories.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', '/home/intellect...', 2, Array)
+#1 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/View.php(61): include('/home/intellect...')
+#2 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/View.php(348): Kohana_View::capture('/home/intellect...', Array)
+#3 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/View.php(228): Kohana_View->render()
+#4 /home/intellectit/Документы/проекты/smartcopy.by/application/views/client/template.php(88): Kohana_View->__toString()
+#5 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/View.php(61): include('/home/intellect...')
+#6 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/View.php(348): Kohana_View::capture('/home/intellect...', Array)
+#7 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/Controller/Template.php(44): Kohana_View->render()
+#8 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/Controller.php(87): Kohana_Controller_Template->after()
+#9 [internal function]: Kohana_Controller->execute()
+#10 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Client))
+#11 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#12 /home/intellectit/Документы/проекты/smartcopy.by/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#13 /home/intellectit/Документы/проекты/smartcopy.by/index.php(118): Kohana_Request->execute()
+#14 {main} in /home/intellectit/Документы/проекты/smartcopy.by/application/views/client/categories.php:2
