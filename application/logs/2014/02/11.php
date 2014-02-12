904ae08c306d87e74447188cbfd5197865e4caa7
veronika89 <hamn@mail.ru>
2014-02-12 10:57:29 +0200

    bbb

--- application/logs/2014/02/11.php
+++ application/logs/2014/02/11.php
@@ -533,3 +533,38 @@
 #13 /home/intellectit/Документы/проекты/smartcopy.by/index.php(118): Kohana_Request->execute()
 #14 {main} in /home/intellectit/Документы/проекты/smartcopy.by/application/views/client/contacts.php:40
 >>>>>>> 27efe53dbc19cc9a7b01e0143dbabedf5c3b842b
+
+2014-02-11 23:09:47 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected ' ~ APPPATH\classes\Controller\Client.php [ 164 ] in file:line
+2014-02-11 23:09:47 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-02-11 23:09:50 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected ' ~ APPPATH\classes\Controller\Client.php [ 164 ] in file:line
+2014-02-11 23:09:50 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-02-11 23:09:54 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected ' ~ APPPATH\classes\Controller\Client.php [ 164 ] in file:line
+2014-02-11 23:09:54 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-02-11 23:09:59 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected ' ~ APPPATH\classes\Controller\Client.php [ 164 ] in file:line
+2014-02-11 23:09:59 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-02-11 23:10:04 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected ' ~ APPPATH\classes\Controller\Client.php [ 164 ] in file:line
+2014-02-11 23:10:04 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-02-11 23:11:12 --- CRITICAL: ErrorException [ 8 ]: Undefined variable: zapravka ~ APPPATH\views\client\uslugi.php [ 28 ] in D:\veronika\smart_copy\application\views\client\uslugi.php:28
+2014-02-11 23:11:12 --- DEBUG: #0 D:\veronika\smart_copy\application\views\client\uslugi.php(28): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\veronika\sma...', 28, Array)
+#1 D:\veronika\smart_copy\system\classes\Kohana\View.php(61): include('D:\veronika\sma...')
+#2 D:\veronika\smart_copy\system\classes\Kohana\View.php(348): Kohana_View::capture('D:\veronika\sma...', Array)
+#3 D:\veronika\smart_copy\system\classes\Kohana\View.php(228): Kohana_View->render()
+#4 D:\veronika\smart_copy\application\views\client\template.php(88): Kohana_View->__toString()
+#5 D:\veronika\smart_copy\system\classes\Kohana\View.php(61): include('D:\veronika\sma...')
+#6 D:\veronika\smart_copy\system\classes\Kohana\View.php(348): Kohana_View::capture('D:\veronika\sma...', Array)
+#7 D:\veronika\smart_copy\system\classes\Kohana\Controller\Template.php(44): Kohana_View->render()
+#8 D:\veronika\smart_copy\system\classes\Kohana\Controller.php(87): Kohana_Controller_Template->after()
+#9 [internal function]: Kohana_Controller->execute()
+#10 D:\veronika\smart_copy\system\classes\Kohana\Request\Client\Internal.php(97): ReflectionMethod->invoke(Object(Controller_Client))
+#11 D:\veronika\smart_copy\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#12 D:\veronika\smart_copy\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#13 D:\veronika\smart_copy\index.php(118): Kohana_Request->execute()
+#14 {main} in D:\veronika\smart_copy\application\views\client\uslugi.php:28
+2014-02-11 23:11:18 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected 'endforeach' (T_ENDFOREACH) ~ APPPATH\views\client\news.php [ 27 ] in file:line
+2014-02-11 23:11:18 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
