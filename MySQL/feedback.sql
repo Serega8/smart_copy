691901c6bc7ba76ccf82a6fa6988947d1791128d
veronika89 <hamn@mail.ru>
2014-02-13 19:21:38 +0200

    db feedback

--- MySQL/feedback.sql
+++ MySQL/feedback.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.8.1deb1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Янв 30 2014 г., 20:47
--- Версия сервера: 5.5.34-0ubuntu0.13.04.1
--- Версия PHP: 5.4.9-4ubuntu2.4
+-- Время создания: Фев 13 2014 г., 17:19
+-- Версия сервера: 5.6.12-log
+-- Версия PHP: 5.4.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -36,18 +36,23 @@ CREATE TABLE IF NOT EXISTS `feedback` (
   `sort` int(11) NOT NULL DEFAULT '1',
   `status` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Дамп данных таблицы `feedback`
 --
 
 INSERT INTO `feedback` (`id`, `type`, `tag`, `placeholder`, `ident`, `name`, `sort`, `status`) VALUES
-(1, 'text', 'input', '(017) 202-76-89', '', 'телефон', 1, 0),
+(1, 'text', 'input', '', '', '', 1, 3),
 (2, 'tel', 'input', '', '', '', 1, 3),
 (3, 'text', 'input', '', '', '', 1, 3),
-(4, 'text', 'input', 'г. Минск ул. Понамаренко 35-102', '', 'адрес', 1, 0),
-(5, 'text', 'input', 'Пн-Пт 9<sup>00</sup>-20<sup>00</sup>', '', 'время работы', 1, 0);
+(4, 'text', 'input', '', '', '', 1, 3),
+(5, 'text', 'input', '', '', '', 1, 3),
+(6, 'text', 'input', 'Иван Петрович', '', 'Имя', 1, 0),
+(7, 'text', 'input', 'ivan@tut.by', '', 'E-mail', 1, 0),
+(8, 'text', 'input', '+375441234567', '', 'Телефон', 1, 0),
+(9, 'text', 'input', '', '', 'Сообщение', 1, 3),
+(10, '', 'textarea', 'Сообщение', '', 'Сообщение', 1, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
